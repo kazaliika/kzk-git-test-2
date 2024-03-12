3b74bdf131d47cc934d88f721d8cfef1b2118669
kazaliika <mavero811@gmail.com>
2024-03-12 13:59:09 +0700

    tambah paragraf

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coba Git Remote</title>
 </head>
+
 <body>
     <h1>Hello Linggau</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis possimus optio dolore placeat quisquam natus illum omnis excepturi tempore quaerat sed reiciendis perspiciatis iure voluptate vel eligendi, blanditiis itaque beatae.</p>
 </body>
+
 </html>
